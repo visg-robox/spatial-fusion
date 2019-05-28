aee7fd822b52e8deb535ea4ec3f34bea49c54a80
limitime <1148198337@qq.com>
2019-05-28 16:14:15 +0800

    fix
    generate small sequence, fix visual bug

--- data_process/generate_test_set.py
+++ data_process/generate_test_set.py
@@ -4,6 +4,7 @@ import os, shutil
 import common
 import numpy as np
 
+
 def get_file_list(data_dir):
     path_list = list()
     for i in os.listdir(data_dir):
@@ -24,30 +25,44 @@ def to_distance(name, initial_pose):
 if __name__ == "__main__":
     data_path = common.blockfile_path
     infer_path = data_path + 'infer_feature/'
+    infer_label_path = data_path + 'infer_label/'
     gt_path = data_path + 'gt_feature/'
     test_infer_path = data_path + 'test_feature/infer/'
     test_gt_path = data_path + 'test_feature/gt/'
+    test_label_path = data_path + 'test_feature/infer_label/'
     make_path(test_infer_path)
     make_path(test_gt_path)
+    make_path(test_label_path)
 
     #initial_pose = np.loadtxt('./raw.txt')
     initial_pose = np.array([-1.206e+04, 2.755e+03, 3.997e+01])
     infer_file = get_file_list(infer_path)
     infer_file.sort(key=lambda x:(to_distance(x.split('/')[-1].split('.')[0], initial_pose)))
+    infer_label_file = get_file_list(infer_label_path)
+    infer_label_file.sort(key=lambda x: (to_distance(x.split('/')[-1].split('.')[0], initial_pose)))
     gt_file = get_file_list(gt_path)
     gt_file.sort(key=lambda x:(to_distance(x.split('/')[-1].split('.')[0], initial_pose)))
 
-    move_length = len(infer_file)//5
+    divide_num = 5
+    each_divide = len(infer_file) // divide_num
+    move_length = each_divide // 5
 
-    infer_file_move = infer_file[-move_length:]
-    gt_file_move = gt_file[-move_length:]
-    for item in infer_file_move:
-        cur_infer_file = item
-        fpath, fname = os.path.split(cur_infer_file)
-        shutil.move(cur_infer_file, test_infer_path + fname)
+    for i in range(divide_num):
+        infer_file_move = infer_file[(i + 1) * each_divide - move_length: (i + 1) * each_divide]
+        gt_file_move = gt_file[(i + 1) * each_divide - move_length: (i + 1) * each_divide]
+        infer_label_move = infer_label_file[(i + 1) * each_divide - move_length: (i + 1) * each_divide]
+        for item in infer_file_move:
+            cur_infer_file = item
+            fpath, fname = os.path.split(cur_infer_file)
+            shutil.move(cur_infer_file, test_infer_path + fname)
 
-    for item in gt_file_move:
-        cur_gt_file = item
-        fpath, fname = os.path.split(cur_gt_file)
-        shutil.move(cur_gt_file, test_gt_path + fname)
+        for item in gt_file_move:
+            cur_gt_file = item
+            fpath, fname = os.path.split(cur_gt_file)
+            shutil.move(cur_gt_file, test_gt_path + fname)
+
+        for item in infer_label_move:
+            cur_label_file = item
+            fpath, fname = os.path.split(cur_label_file)
+            shutil.move(cur_label_file, test_label_path + fname)
 
--- train/feature/rnn_torch2.py
+++ train/feature/rnn_torch2.py
@@ -9,7 +9,7 @@ import sys
 sys.path.append("/home/zhangjian/code/project/spatial-fusion/")
 import common
 import torch
-
+from os.path import join
 import random
 from model.rnn import *
 
@@ -24,34 +24,42 @@ from tensorboardX import SummaryWriter
 # Hyper Parameters
 EPOCH = 100                             # train the training data n times, to save time, we just train 1 epoch
 # when batch size = 1, we just want to have a test
-BATCH_SIZE = 512  # common.batch_size
-TIME_STEP = 50  # common.time_step                          # rnn time step / image height
-INPUT_SIZE = common.feature_num_new           # rnn input size / image width
-HIDDEN_SIZE = common.feature_num_new
+BATCH_SIZE = 64  # common.batch_size
+TIME_STEP = common.time_step  # common.time_step                          # rnn time step / image height
+INPUT_SIZE = common.feature_num_raw           # rnn input size / image width
+HIDDEN_SIZE = common.feature_num_raw
 OUTPUT_SIZE = common.class_num
 LR = 0.001                              # learning rate
 WINDOW_SIZE = 50
-
+Pretrained = False
+file_num = 3
+model_path = None
 USING_RNN_FEATURE = common.USING_RNN_FEATURE
 USING_SSNet_FEATURE = common.USING_SSNet_FEATURE
 
 
 if __name__ == '__main__':
 
-    data_path = common.data_path
-    infer_path = data_path + 'CARLA_episode_0019/test3/infer_feature/'
-    gt_path = data_path + 'CARLA_episode_0019/test3/gt_feature/'
-    test_infer_path = data_path + 'CARLA_episode_0019/test3/test_feature/infer/'
-    test_gt_path = data_path + 'CARLA_episode_0019/test3/test_feature/gt/'
-    res_save_path = str(os.getcwd()) + '/runs/average_feature/'
+    data_path = common.blockfile_path
+    infer_path = join(data_path, 'infer_feature/')
+    gt_path = join(data_path, 'gt_feature/')
+    test_infer_path = join(data_path, 'test_feature/infer/')
+    test_gt_path = join(data_path, 'test_feature/gt/')
+    res_save_path = str(os.getcwd()) + '/runs/LSTM_imgfeature/'
+    if not os.path.isdir(res_save_path):
+        os.makedirs(res_save_path)
 
     infer_file = get_file_list(infer_path)
     infer_file.sort()
     gt_file = get_file_list(gt_path)
     gt_file.sort()
 
-    writer = SummaryWriter('runs/average_feature')
-    rnn = SSNet(INPUT_SIZE, HIDDEN_SIZE, OUTPUT_SIZE)
+    writer = SummaryWriter(res_save_path)
+    if Pretrained == False:
+        rnn = SSNet(INPUT_SIZE, HIDDEN_SIZE, OUTPUT_SIZE)
+    else:
+        rnn = torch.load(model_path)
+
     optimizer = torch.optim.Adam(rnn.parameters(), lr=LR, weight_decay=1e-5)
     loss_func = nn.CrossEntropyLoss()
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.9)
@@ -59,11 +67,14 @@ if __name__ == '__main__':
     random.seed(10)
     rnn.cuda()
     record_iter = 0
+    label_p = np.loadtxt('../../data_process/data_dropout_ratio.txt')
     for epoch in range(EPOCH):
         scheduler.step()
         print('Epoch: ', epoch)
-        for time in range(len(infer_file)//1):
-            file_idx_list = random.sample(range(len(infer_file)), 1)
+        file_idx_list_all = np.arange(len(infer_file))
+        random.shuffle(file_idx_list_all)
+        for time in range(len(infer_file)//file_num):
+            file_idx_list = file_idx_list_all[time * file_num: (time + 1) * file_num]
             voxel_dict = dict()
             gt_dict = dict()
             print('start reading file')
@@ -72,11 +83,13 @@ if __name__ == '__main__':
                 gt_filename = gt_file[file_idx]
                 voxel_dict.update(np.load(infer_filename).item())
                 gt_dict.update(np.load(gt_filename).item())
+            voxel_dict, gt_dict = data_balance.data_balance_rnn_new(voxel_dict, gt_dict, label_p)
             keys_list = get_common_keys(voxel_dict, gt_dict)
             print('finish reading file')
 
+            random.shuffle(keys_list)
             for i in range(len(keys_list)//BATCH_SIZE):
-                current_keys = random.sample(keys_list, BATCH_SIZE)
+                current_keys = keys_list[i * BATCH_SIZE:(i + 1) * BATCH_SIZE]
                 input_data = data_loader_torch.featuremap_to_batch_new(voxel_dict, current_keys, BATCH_SIZE, TIME_STEP, INPUT_SIZE)
                 input_data = Variable(input_data, requires_grad=True).cuda()
                 gt = data_loader_torch.featuremap_to_gt_num(gt_dict, current_keys, BATCH_SIZE, ignore_list=common.ignore_list)
@@ -84,6 +97,7 @@ if __name__ == '__main__':
 
                 output = rnn(input_data, TIME_STEP)
                 loss = loss_func(output, gt)
+                print(loss)
                 optimizer.zero_grad()
                 loss.backward()
                 # for name, param in rnn.named_parameters():
@@ -92,9 +106,9 @@ if __name__ == '__main__':
                 record_iter += 1
                 writer.add_scalar('data/feature_training_loss', loss, record_iter)
                 print(record_iter)
-                if record_iter % 5000 == 0:
+                if record_iter % 2500 == 0:
                     model_name = res_save_path + str(record_iter) + '_model.pkl'
                     torch.save(rnn, model_name)
-                    eval_ssnet(test_infer_path, test_gt_path, model_name, res_save_path, WINDOW_SIZE, time_step=TIME_STEP, log_dir=res_save_path)
+                    #eval_ssnet(test_infer_path, test_gt_path, model_name, res_save_path, WINDOW_SIZE, time_step=TIME_STEP, log_dir=res_save_path)
     writer.close()
 
